983f40e99b55b27df0d01859ce96ff9c7215d6f1
Dishendra <dishendramishra@gmail.com>
2019-06-27 15:22:59 +0530

    now reads number of lines automatically

--- dishendra_plot.py
+++ dishendra_plot.py
@@ -1,23 +1,34 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+import subprocess
 
-rows, cols = 1031340, 2
+rows = int(subprocess.check_output(["wc","-l","dishendra.txt"]).decode("utf-8").split()[0])
+cols = 2
 
 points = np.memmap('memmapped.dat', dtype=np.float32,
               mode='w+', shape=(rows, cols))
 
 values = None
-with open("dishendra.txt","r") as f:
-    for row in range(rows):
-        try:
-            temp = f.readline().split()
-            points[row][0] = temp[0]  # x value
-            points[row][1] = temp[1]  # y value
-        except:
-            pass
+row = 0
 
-# print points[:,0]     # x values
-# print points[:,1]     # y values
+with open("dishendra.txt","r") as f:
+    while True:
+        temp = f.readline()
+        if(temp == ""):
+            print "End of File reached!"
+            break
+        else:
+            temp = temp.split()
+            try:
+                points[row][0] = temp[0]  # x value
+                points[row][1] = temp[1]  # y value
+            except:
+                pass
+        row += 1
+
+# print points[:,0]     #
+# print points[:,1]     #
 
 plt.plot(points[:,0],points[:, 1])
 plt.show()
